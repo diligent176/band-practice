c3a84d44b9b07f99595313d380ac56dee55949c4
Jason Bellis <jason.bellis@veritasdataresearch.com>
2025-10-04 14:20:37 -0700

    tf dumb

--- terraform/main.tf
+++ terraform/main.tf
@@ -177,16 +177,18 @@ resource "google_service_account" "cloud_run_sa" {
 
 # Grant Firestore access to service account
 resource "google_project_iam_member" "firestore_user" {
-  project = var.project_id
-  role    = "roles/datastore.user"
-  member  = "serviceAccount:${google_service_account.cloud_run_sa.email}"
+  project    = var.project_id
+  role       = "roles/datastore.user"
+  member     = "serviceAccount:${google_service_account.cloud_run_sa.email}"
+  depends_on = [google_service_account.cloud_run_sa]
 }
 
 # Grant Secret Manager access to service account
 resource "google_project_iam_member" "secret_accessor" {
-  project = var.project_id
-  role    = "roles/secretmanager.secretAccessor"
-  member  = "serviceAccount:${google_service_account.cloud_run_sa.email}"
+  project    = var.project_id
+  role       = "roles/secretmanager.secretAccessor"
+  member     = "serviceAccount:${google_service_account.cloud_run_sa.email}"
+  depends_on = [google_service_account.cloud_run_sa, google_project_service.secretmanager]
 }
 
 # Outputs
