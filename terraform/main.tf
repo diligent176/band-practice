71d288b0e5e5d94baaf9ca7057a7da8ffc842e5d
Jason Bellis <jason.bellis@veritasdataresearch.com>
2025-10-05 08:17:37 -0700

    add more secrets

--- terraform/main.tf
+++ terraform/main.tf
@@ -46,13 +46,24 @@ resource "google_project_service" "secretmanager" {
 }
 
 # Secret Manager Secrets
-resource "google_secret_manager_secret" "spotify_client_secret" {
-  secret_id = "SPOTIFY_CLIENT_SECRET"
-
+resource "google_secret_manager_secret" "spotify_client_id" {
+  secret_id = "SPOTIFY_CLIENT_ID"
   replication {
     auto {}
   }
+  depends_on = [google_project_service.secretmanager]
+}
 
+resource "google_secret_manager_secret_version" "spotify_client_id" {
+  secret      = google_secret_manager_secret.spotify_client_id.id
+  secret_data = var.spotify_client_id
+}
+
+resource "google_secret_manager_secret" "spotify_client_secret" {
+  secret_id = "SPOTIFY_CLIENT_SECRET"
+  replication {
+    auto {}
+  }
   depends_on = [google_project_service.secretmanager]
 }
 
@@ -61,18 +72,55 @@ resource "google_secret_manager_secret_version" "spotify_client_secret" {
   secret_data = var.spotify_client_secret
 }
 
-resource "google_secret_manager_secret" "allowed_user_emails" {
-  secret_id = "ALLOWED_USER_EMAILS"
-
+resource "google_secret_manager_secret" "genius_access_token" {
+  secret_id = "GENIUS_ACCESS_TOKEN"
   replication {
     auto {}
   }
-
   depends_on = [google_project_service.secretmanager]
 }
 
-resource "google_secret_manager_secret_version" "allowed_user_emails" {
-  secret      = google_secret_manager_secret.allowed_user_emails.id
+resource "google_secret_manager_secret_version" "genius_access_token" {
+  secret      = google_secret_manager_secret.genius_access_token.id
+  secret_data = var.genius_access_token
+}
+
+resource "google_secret_manager_secret" "secret_key" {
+  secret_id = "SECRET_KEY"
+  replication {
+    auto {}
+  }
+  depends_on = [google_project_service.secretmanager]
+}
+
+resource "google_secret_manager_secret_version" "secret_key" {
+  secret      = google_secret_manager_secret.secret_key.id
+  secret_data = var.flask_secret_key
+}
+
+resource "google_secret_manager_secret" "firebase_api_key" {
+  secret_id = "FIREBASE_API_KEY"
+  replication {
+    auto {}
+  }
+  depends_on = [google_project_service.secretmanager]
+}
+
+resource "google_secret_manager_secret_version" "firebase_api_key" {
+  secret      = google_secret_manager_secret.firebase_api_key.id
+  secret_data = var.firebase_api_key
+}
+
+resource "google_secret_manager_secret" "allowed_users" {
+  secret_id = "ALLOWED_USERS"
+  replication {
+    auto {}
+  }
+  depends_on = [google_project_service.secretmanager]
+}
+
+resource "google_secret_manager_secret_version" "allowed_users" {
+  secret      = google_secret_manager_secret.allowed_users.id
   secret_data = join(",", var.allowed_user_emails)
 }
 
@@ -106,39 +154,15 @@ resource "google_cloud_run_service" "band_practice" {
       containers {
         image = "${var.region}-docker.pkg.dev/${var.project_id}/band-practice-pro/band-practice-pro:latest"
 
+        # Non-sensitive env vars
         env {
           name  = "GCP_PROJECT_ID"
           value = var.project_id
         }
 
         env {
-          name  = "SPOTIFY_CLIENT_ID"
-          value = var.spotify_client_id
-        }
-
-        env {
-          name  = "SPOTIFY_CLIENT_SECRET"
-          value = var.spotify_client_secret
-        }
-
-        env {
-          name  = "GENIUS_ACCESS_TOKEN"
-          value = var.genius_access_token
-        }
-
-        env {
           name  = "SPOTIFY_PLAYLIST_URL"
           value = var.spotify_playlist_url
-        }
-
-        env {
-          name  = "SECRET_KEY"
-          value = var.flask_secret_key
-        }
-
-        env {
-          name  = "FIREBASE_API_KEY"
-          value = var.firebase_api_key
         }
 
         env {
@@ -151,9 +175,65 @@ resource "google_cloud_run_service" "band_practice" {
           value = var.project_id
         }
 
+        # Secrets mounted from Secret Manager
         env {
-          name  = "ALLOWED_USERS"
-          value = join(",", var.allowed_user_emails)
+          name = "SPOTIFY_CLIENT_ID"
+          value_from {
+            secret_key_ref {
+              name = google_secret_manager_secret.spotify_client_id.secret_id
+              key  = "latest"
+            }
+          }
+        }
+
+        env {
+          name = "SPOTIFY_CLIENT_SECRET"
+          value_from {
+            secret_key_ref {
+              name = google_secret_manager_secret.spotify_client_secret.secret_id
+              key  = "latest"
+            }
+          }
+        }
+
+        env {
+          name = "GENIUS_ACCESS_TOKEN"
+          value_from {
+            secret_key_ref {
+              name = google_secret_manager_secret.genius_access_token.secret_id
+              key  = "latest"
+            }
+          }
+        }
+
+        env {
+          name = "SECRET_KEY"
+          value_from {
+            secret_key_ref {
+              name = google_secret_manager_secret.secret_key.secret_id
+              key  = "latest"
+            }
+          }
+        }
+
+        env {
+          name = "FIREBASE_API_KEY"
+          value_from {
+            secret_key_ref {
+              name = google_secret_manager_secret.firebase_api_key.secret_id
+              key  = "latest"
+            }
+          }
+        }
+
+        env {
+          name = "ALLOWED_USERS"
+          value_from {
+            secret_key_ref {
+              name = google_secret_manager_secret.allowed_users.secret_id
+              key  = "latest"
+            }
+          }
         }
 
         resources {
