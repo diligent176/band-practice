48a170acc74f6322e820c0f86d6ce65c7dd631ff
Jason Bellis <jason.bellis@veritasdataresearch.com>
2025-10-13 16:49:14 -0700

    create index for playlist_memory

--- terraform/firestore.tf
+++ terraform/firestore.tf
@@ -8,3 +8,23 @@ resource "google_firestore_database" "database" {
 
   depends_on = [google_project_service.firestore]
 }
+
+# Composite index for playlist_memory collection
+# Enables efficient querying of playlists by user_id, sorted by last_accessed
+resource "google_firestore_index" "playlist_memory_user_last_accessed" {
+  project    = var.project_id
+  database   = google_firestore_database.database.name
+  collection = "playlist_memory"
+
+  fields {
+    field_path = "user_id"
+    order      = "ASCENDING"
+  }
+
+  fields {
+    field_path = "last_accessed"
+    order      = "DESCENDING"
+  }
+
+  depends_on = [google_firestore_database.database]
+}
