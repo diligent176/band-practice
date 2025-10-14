84734c6faa68e5224853a7b37d4ae2b7a5c633f3
Jason Bellis <jason.bellis@veritasdataresearch.com>
2025-10-13 19:37:03 -0700

    more firestore indexes

--- terraform/firestore.tf
+++ terraform/firestore.tf
@@ -28,3 +28,48 @@ resource "google_firestore_index" "playlist_memory_user_last_accessed" {
 
   depends_on = [google_firestore_database.database]
 }
+
+# Composite index for collections collection
+# Enables efficient querying of collections by user_id, sorted by name
+resource "google_firestore_index" "collections_user_name" {
+  project    = var.project_id
+  database   = google_firestore_database.database.name
+  collection = "collections"
+
+  fields {
+    field_path = "user_id"
+    order      = "ASCENDING"
+  }
+
+  fields {
+    field_path = "name"
+    order      = "ASCENDING"
+  }
+
+  depends_on = [google_firestore_database.database]
+}
+
+# Composite index for songs collection by collection_id
+# Enables efficient querying of songs by collection_id, sorted by artist and title
+resource "google_firestore_index" "songs_collection_artist_title" {
+  project    = var.project_id
+  database   = google_firestore_database.database.name
+  collection = "songs"
+
+  fields {
+    field_path = "collection_id"
+    order      = "ASCENDING"
+  }
+
+  fields {
+    field_path = "artist"
+    order      = "ASCENDING"
+  }
+
+  fields {
+    field_path = "title"
+    order      = "ASCENDING"
+  }
+
+  depends_on = [google_firestore_database.database]
+}
